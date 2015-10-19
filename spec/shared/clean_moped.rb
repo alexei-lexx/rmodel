2a3585215093a34e7b0f492d5857d535db28883d
Alexei Lexx <alexei.lexx@gmail.com>
2015-10-19 17:09:22 +0300

    refactor specs

--- spec/shared/clean_moped.rb
+++ spec/shared/clean_moped.rb
@@ -1,8 +1,10 @@
 RSpec.shared_context 'clean mongodb database' do
+  let(:mongo_session) { Mongo::Client.new([ '127.0.0.1:27017' ], database: 'rmodel_test') }
+
   before(:all) do
     Mongo::Logger.logger.level = Logger::ERROR
-    @mongo_client = Mongo::Client.new([ '127.0.0.1:27017' ], database: 'rmodel_test')
-    @mongo_client.database.drop
+    mongo_session = Mongo::Client.new([ '127.0.0.1:27017' ], database: 'rmodel_test')
+    mongo_session.database.drop
   end
-  after { @mongo_client.database.drop }
+  after { mongo_session.database.drop }
 end
