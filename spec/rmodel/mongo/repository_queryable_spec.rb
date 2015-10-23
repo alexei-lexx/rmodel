722f242e40df13c950191ebc2decbf7d78e6f4b8
Alexei L <alexei.lexx@gmail.com>
2015-10-23 23:09:30 +0300

    switch Rmode::Mongo::Repository to client instead of session

--- spec/rmodel/mongo/repository_queryable_spec.rb
+++ spec/rmodel/mongo/repository_queryable_spec.rb
@@ -3,6 +3,9 @@ RSpec.describe Rmodel::Mongo::Repository do
 
   before do
     Rmodel.sessions[:default] = mongo_session
+    Rmodel.setup do
+      client :default, hosts: [ 'localhost' ], database: 'rmodel_test'
+    end
     stub_const('Thing', Struct.new(:id, :a, :b))
     stub_const('ThingRepository', Class.new(Rmodel::Mongo::Repository) {
       simple_factory Thing, :a, :b
