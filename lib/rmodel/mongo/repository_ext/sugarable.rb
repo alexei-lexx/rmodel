a39797e8e9e29fb62adc47c7d8c2c52d7ae79531
Alexei L <alexei.lexx@gmail.com>
2015-10-25 22:42:21 +0200

    add Rmodel::Mongo::Sugarable#save

--- lib/rmodel/mongo/repository_ext/sugarable.rb
+++ lib/rmodel/mongo/repository_ext/sugarable.rb
@@ -4,6 +4,14 @@ module Rmodel::Mongo
       def find!(id)
         find(id) or raise Rmodel::NotFound.new(self, { id: id })
       end
+
+      def save(object)
+        if object.id.nil?
+          insert(object)
+        else
+          update(object)
+        end
+      end
     end
   end
 end
