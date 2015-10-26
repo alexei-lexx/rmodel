01950af7bc2acf6f598b356db7d15fdb307d686e
Alexei L <alexei.lexx@gmail.com>
2015-10-26 18:10:10 +0200

    refactor spec shared examples

--- spec/rmodel/mongo/repository_ext/sugarable_spec.rb
+++ spec/rmodel/mongo/repository_ext/sugarable_spec.rb
@@ -1,14 +1,14 @@
 RSpec.describe Rmodel::Mongo::Repository do
-  include_context 'clean Mongo database'
+  include_examples 'sugarable repository' do
+    include_context 'clean Mongo database'
 
-  before do
-    stub_const('ThingRepository', Class.new(Rmodel::Mongo::Repository))
+    before do
+      stub_const('ThingRepository', Class.new(Rmodel::Mongo::Repository))
+    end
+
+    subject do
+      factory = Rmodel::Mongo::SimpleFactory.new(Thing, :name)
+      ThingRepository.new(mongo_session, :things, factory)
+    end
   end
-
-  subject do
-    factory = Rmodel::Mongo::SimpleFactory.new(Thing, :name)
-    ThingRepository.new(mongo_session, :things, factory)
-  end
-
-  include_examples 'sugarable repository'
 end
--- spec/rmodel/mongo/repository_ext/timestampable_spec.rb
+++ spec/rmodel/mongo/repository_ext/timestampable_spec.rb
@@ -1,19 +1,19 @@
 RSpec.describe Rmodel::Mongo::Repository do
-  include_context 'clean Mongo database'
+  include_examples 'timestampable repository' do
+    include_context 'clean Mongo database'
 
-  before do
-    stub_const('ThingRepository', Class.new(Rmodel::Mongo::Repository))
+    before do
+      stub_const('ThingRepository', Class.new(Rmodel::Mongo::Repository))
+    end
+
+    let(:repo_w_timestamps) do
+      factory = Rmodel::Mongo::SimpleFactory.new(Thing, :name, :created_at, :updated_at)
+      ThingRepository.new(mongo_session, :things, factory)
+    end
+
+    let(:repo_wo_timestamps) do
+      factory = Rmodel::Mongo::SimpleFactory.new(Thing, :name)
+      ThingRepository.new(mongo_session, :things, factory)
+    end
   end
-
-  let(:repo_w_timestamps) do
-    factory = Rmodel::Mongo::SimpleFactory.new(Thing, :name, :created_at, :updated_at)
-    ThingRepository.new(mongo_session, :things, factory)
-  end
-
-  let(:repo_wo_timestamps) do
-    factory = Rmodel::Mongo::SimpleFactory.new(Thing, :name)
-    ThingRepository.new(mongo_session, :things, factory)
-  end
-
-  include_examples 'timestampable repository'
 end
