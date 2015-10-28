2d0cea637bcaaf0f7218f05025f839f05c5f9222
Alexei L <alexei.lexx@gmail.com>
2015-10-28 16:12:49 +0200

    refactor repository specs

--- spec/rmodel/mongo/repository_spec.rb
+++ spec/rmodel/mongo/repository_spec.rb
@@ -29,4 +29,22 @@ RSpec.describe Rmodel::Mongo::Repository do
       ThingRepository.new(mongo_session, :things, factory)
     end
   end
+
+  it_behaves_like 'timestampable repository' do
+    include_context 'clean mongo database'
+
+    before do
+      stub_const('ThingRepository', Class.new(Rmodel::Mongo::Repository))
+    end
+
+    let(:repo_w_timestamps) do
+      factory = Rmodel::Mongo::SimpleFactory.new(Thing, :name, :created_at, :updated_at)
+      ThingRepository.new(mongo_session, :things, factory)
+    end
+
+    let(:repo_wo_timestamps) do
+      factory = Rmodel::Mongo::SimpleFactory.new(Thing, :name)
+      ThingRepository.new(mongo_session, :things, factory)
+    end
+  end
 end
