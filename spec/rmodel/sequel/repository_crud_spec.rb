f505433d123f7c32cd0284d4f395a43d36013845
Alexei L <alexei.lexx@gmail.com>
2015-10-28 16:01:15 +0200

    add Sugarable to Rmodel::Sequel::Repository

--- spec/rmodel/sequel/repository_crud_spec.rb
+++ spec/rmodel/sequel/repository_crud_spec.rb
@@ -3,11 +3,7 @@ RSpec.describe Rmodel::Sequel::Repository do
     include_examples 'clean sequel database'
 
     before do
-      sequel_conn.create_table(:things) do
-        primary_key :id
-        String :name
-      end
-
+      create_database
       stub_const('ThingRepository', Class.new(Rmodel::Sequel::Repository))
     end
 
@@ -19,6 +15,27 @@ RSpec.describe Rmodel::Sequel::Repository do
       record = columns.dup
       record[:id] = id
       sequel_conn[:things].insert(record)
+    end
+  end
+
+  it_behaves_like 'sugarable repository' do
+    include_examples 'clean sequel database'
+
+    before do
+      create_database
+      stub_const('ThingRepository', Class.new(Rmodel::Sequel::Repository))
+    end
+
+    subject do
+      factory = Rmodel::Sequel::SimpleFactory.new(Thing, :name)
+      ThingRepository.new(sequel_conn, :things, factory)
+    end
+  end
+
+  def create_database
+    sequel_conn.create_table(:things) do
+      primary_key :id
+      String :name
     end
   end
 end
