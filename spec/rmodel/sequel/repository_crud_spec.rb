411f2359946fcd756945d84725d50e8b386801ab
Alexei L <alexei.lexx@gmail.com>
2015-10-28 16:09:43 +0200

    add Timestampable to Rmodel::Sequel::Repository

--- spec/rmodel/sequel/repository_crud_spec.rb
+++ spec/rmodel/sequel/repository_crud_spec.rb
@@ -32,10 +32,33 @@ RSpec.describe Rmodel::Sequel::Repository do
     end
   end
 
-  def create_database
+  it_behaves_like 'timestampable repository' do
+    include_examples 'clean sequel database'
+
+    before do
+      create_database(true)
+      stub_const('ThingRepository', Class.new(Rmodel::Sequel::Repository))
+    end
+
+    let(:repo_w_timestamps) do
+      factory = Rmodel::Sequel::SimpleFactory.new(Thing, :name, :created_at, :updated_at)
+      ThingRepository.new(sequel_conn, :things, factory)
+    end
+
+    let(:repo_wo_timestamps) do
+      factory = Rmodel::Sequel::SimpleFactory.new(Thing, :name)
+      ThingRepository.new(sequel_conn, :things, factory)
+    end
+  end
+
+  def create_database(timestamps = false)
     sequel_conn.create_table(:things) do
       primary_key :id
       String :name
+      if timestamps
+        Time :created_at
+        Time :updated_at
+      end
     end
   end
 end
