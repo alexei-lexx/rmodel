50a6b5c11f62d45441e6cd039d0b9b7655741454
Alexei <alexei.lexx@gmail.com>
2015-10-29 14:15:38 +0200

    simplify example

--- examples/mongo_embedded.rb
+++ examples/mongo_embedded.rb
@@ -5,28 +5,14 @@ Rmodel.setup do
 end
 
 Owner = Struct.new(:id, :first_name, :last_name)
-
 Room = Struct.new(:id, :name, :square)
-
 Flat = Struct.new(:id, :address, :rooms, :owner)
-
-class OwnerFactory < Rmodel::Mongo::SimpleFactory
-  def initialize
-    super Owner, :first_name, :last_name
-  end
-end
-
-class RoomFactory < Rmodel::Mongo::SimpleFactory
-  def initialize
-    super Room, :name, :square
-  end
-end
 
 class FlatFactory < Rmodel::Mongo::SimpleFactory
   def initialize
     super Flat, :address
-    embeds_many :rooms, RoomFactory.new
-    embeds_one :owner, OwnerFactory.new
+    embeds_many :rooms, Rmodel::Mongo::SimpleFactory.new(Room, :name, :square)
+    embeds_one :owner, Rmodel::Mongo::SimpleFactory.new(Owner, :first_name, :last_name)
   end
 end
 
