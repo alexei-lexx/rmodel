595cceda24c7501339228324e931183be704169f
Alexei <alexei.lexx@gmail.com>
2015-10-29 15:25:53 +0200

    rewrite Rmodel::Mongo::SimpleFactory specs

--- spec/rmodel/mongo/simple_factory_spec.rb
+++ spec/rmodel/mongo/simple_factory_spec.rb
@@ -1,41 +1,41 @@
 RSpec.describe Rmodel::Mongo::SimpleFactory do
-  context 'when the User(id, name, email) class is defined' do
-    before { stub_const('User', Struct.new(:id, :name, :email)) }
+  context 'when the Thing(id, name, price) class is defined' do
+    before { stub_const('Thing', Struct.new(:id, :name, :price)) }
 
-    subject(:factory) { described_class.new(User, :name, :email) }
+    subject(:factory) { described_class.new(Thing, :name, :price) }
 
     describe '#fromHash' do
-      context 'when the hash with _id, name and email is given' do
-        let(:hash) { { '_id' => 1, 'name' => 'John', 'email' => 'john@example.com' } }
+      context 'when the hash with _id, name and price is given' do
+        let(:hash) { { '_id' => 1, 'name' => 'chair', 'price' => 100 } }
         let(:result) { factory.fromHash(hash) }
 
-        it 'returns an instance of User' do
-          expect(result).to be_an_instance_of User
+        it 'returns an instance of Thing' do
+          expect(result).to be_an_instance_of Thing
         end
 
         it 'sets the attributes correctly' do
-          expect(result.name).to eq 'John'
-          expect(result.email).to eq 'john@example.com'
+          expect(result.name).to eq 'chair'
+          expect(result.price).to eq 100
         end
 
-        it 'sets the User#id correctly' do
+        it 'sets the Thing#id correctly' do
           expect(result.id).to eq 1
         end
       end
     end
 
     describe '#toHash' do
-      let(:user) { User.new(1, 'John', 'john@example.com') }
+      let(:thing) { Thing.new(1, 'chair', 100) }
       context 'when id_included is false' do
-        let(:result) { factory.toHash(user, false) }
+        let(:result) { factory.toHash(thing, false) }
 
         it 'returns an instance of Hash' do
           expect(result).to be_an_instance_of Hash
         end
 
         it 'sets the keys correctly' do
-          expect(result['name']).to eq 'John'
-          expect(result['email']).to eq 'john@example.com'
+          expect(result['name']).to eq 'chair'
+          expect(result['price']).to eq 100
         end
 
         it 'has no the "_id" key' do
@@ -44,7 +44,7 @@ RSpec.describe Rmodel::Mongo::SimpleFactory do
       end
 
       context 'when id_included is true' do
-        let(:result) { factory.toHash(user, true) }
+        let(:result) { factory.toHash(thing, true) }
 
         it 'sets the "_id" key' do
           expect(result['_id']).to eq 1
