d294b88e29cd707660962e58a902cbfba19420e2
Alexei <alexei.lexx@gmail.com>
2015-11-17 15:56:26 +0200

    add #primary_key to mappers

--- lib/rmodel/sequel/mapper.rb
+++ lib/rmodel/sequel/mapper.rb
@@ -2,7 +2,7 @@ module Rmodel::Sequel
   class Mapper < Rmodel::Base::Mapper
     def deserialize(hash)
       object = model.new
-      object.id = hash[:id]
+      object.id = hash[primary_key]
       attributes.each do |attribute|
         object.public_send "#{attribute}=", hash[attribute.to_sym]
       end
@@ -15,7 +15,7 @@ module Rmodel::Sequel
         hash[attribute.to_sym] = object.public_send(attribute)
       end
       if id_included
-        hash[:id] = object.id
+        hash[primary_key] = object.id
       end
       hash
     end
