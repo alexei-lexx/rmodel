51f8a38f6e5de4c70e1b5e0bcfe51c94927a5ea7
Alexei L <alexei.lexx@gmail.com>
2015-11-17 14:49:05 +0200

    refactor mapper specs

--- spec/rmodel/sequel/mapper_spec.rb
+++ spec/rmodel/sequel/mapper_spec.rb
@@ -59,29 +59,5 @@ RSpec.describe Rmodel::Sequel::Mapper do
     end
   end
 
-  describe '#initialize' do
-    context 'when the model is not declared' do
-      before do
-        stub_const 'UserMapper', Class.new(described_class)
-      end
-
-      context 'but the model class exists' do
-        it 'is guessed by convention' do
-          expect {
-            subject
-          }.not_to raise_error
-        end
-      end
-
-      context 'and the model class does not exist' do
-        before { hide_const('User') }
-
-        it 'raises an error' do
-          expect {
-            subject
-          }.to raise_error ArgumentError
-        end
-      end
-    end
-  end
+  it_behaves_like 'base mapper'
 end
