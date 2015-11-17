76ae11a15636f63d0934919c7f9ede22045caef7
Alexei <alexei.lexx@gmail.com>
2015-11-17 14:20:49 +0200

    refactor repositories

--- lib/rmodel/mongo/repository.rb
+++ lib/rmodel/mongo/repository.rb
@@ -6,16 +6,13 @@ module Rmodel::Mongo
     include RepositoryExt::Queryable
 
     def initialize(client = nil, collection = nil, mapper = nil)
+      super(mapper)
       @client = client || Rmodel.setup.establish_mongo_client(self.class.client_name || :default) or
                 raise ArgumentError.new('Client driver is not setup')
 
       @collection = collection || self.class.setting_collection ||
                     self.class.collection_by_convention or
                     raise ArgumentError.new('Collection can not be guessed')
-
-      @mapper = mapper || self.class.declared_mapper ||
-                self.class.mapper_by_convention or
-                raise ArgumentError.new('Mapper can not be guessed')
     end
 
     def find(id)
@@ -37,7 +34,7 @@ module Rmodel::Mongo
     end
 
     class << self
-      attr_reader :client_name, :setting_collection, :declared_mapper
+      attr_reader :client_name, :setting_collection
 
       def client(name)
         @client_name = name
@@ -51,18 +48,6 @@ module Rmodel::Mongo
         if name =~ /(.*)Repository$/
           ActiveSupport::Inflector.tableize($1).to_sym
         end
-      end
-
-      def mapper_by_convention
-        if name =~ /(.*)Repository$/
-          ActiveSupport::Inflector.constantize($1 + 'Mapper').new
-        end
-      rescue NameError
-        nil
-      end
-
-      def mapper(mapper_klass)
-        @declared_mapper = mapper_klass.new
       end
     end
   end
--- lib/rmodel/sequel/repository.rb
+++ lib/rmodel/sequel/repository.rb
@@ -6,16 +6,13 @@ module Rmodel::Sequel
     include RepositoryExt::Queryable
 
     def initialize(client = nil, table = nil, mapper = nil)
+      super(mapper)
       @client = client || Rmodel.setup.establish_sequel_client(self.class.client_name || :default) or
                 raise ArgumentError.new('Client driver is not setup')
 
       @table = table || self.class.setting_table ||
                self.class.table_by_convention or
                raise ArgumentError.new('Table can not be guessed')
-
-      @mapper = mapper || self.class.declared_mapper ||
-                self.class.mapper_by_convention or
-                raise ArgumentError.new('Mapper can not be guessed')
     end
 
     def find(id)
@@ -37,7 +34,7 @@ module Rmodel::Sequel
     end
 
     class << self
-      attr_reader :client_name, :setting_table, :declared_mapper
+      attr_reader :client_name, :setting_table
 
       def client(name)
         @client_name = name
@@ -51,18 +48,6 @@ module Rmodel::Sequel
         if name =~ /(.*)Repository$/
           ActiveSupport::Inflector.tableize($1).to_sym
         end
-      end
-
-      def mapper_by_convention
-        if name =~ /(.*)Repository$/
-          ActiveSupport::Inflector.constantize($1 + 'Mapper').new
-        end
-      rescue NameError
-        nil
-      end
-
-      def mapper(mapper_klass)
-        @declared_mapper = mapper_klass.new
       end
     end
   end
