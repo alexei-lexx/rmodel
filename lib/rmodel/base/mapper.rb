df85239511eb387595121bf2097255ce5ff136d9
Alexei L <alexei.lexx@gmail.com>
2015-11-17 15:08:38 +0200

    refactor mappers

---
+++ lib/rmodel/base/mapper.rb
@@ -0,0 +1,31 @@
+module Rmodel::Base
+  class Mapper
+    def initialize
+      if model.nil?
+        raise ArgumentError.new('Model was not declared')
+      end
+    end
+
+    private
+
+    def model
+      self.class.declared_model || self.class.model_by_convention
+    end
+
+    class << self
+      attr_reader :declared_model
+
+      def model(klass)
+        @declared_model = klass
+      end
+
+      def model_by_convention
+        if name =~ /(.*)Mapper$/
+          ActiveSupport::Inflector.constantize($1).new
+        end
+      rescue NameError
+        nil
+      end
+    end
+  end
+end
