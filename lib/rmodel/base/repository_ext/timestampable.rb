02688b453cd60547e274d7d938445e0133dfc0f2
acurguzchin <acurguzchin@cogniance.com>
2015-11-19 18:15:13 +0200

    improve Timestampable

--- lib/rmodel/base/repository_ext/timestampable.rb
+++ lib/rmodel/base/repository_ext/timestampable.rb
@@ -3,17 +3,25 @@ module Rmodel
     module RepositoryExt
       module Timestampable
         def insert_one(object)
-          if object.respond_to?(:created_at) && object.respond_to?(:created_at=)
-            object.created_at ||= Time.now
-          end
+          object.created_at = Time.now if able_to_set_created_at?(object)
           super
         end
 
         def update(object)
-          if object.respond_to?(:updated_at) && object.respond_to?(:updated_at=)
-            object.updated_at = Time.now
-          end
+          object.updated_at = Time.now if able_to_set_updated_at?(object)
           super
+        end
+
+        private
+
+        def able_to_set_created_at?(object)
+          object.respond_to?(:created_at=) &&
+            object.respond_to?(:created_at) &&
+            object.created_at.nil?
+        end
+
+        def able_to_set_updated_at?(object)
+          object.respond_to?(:updated_at=)
         end
       end
     end
