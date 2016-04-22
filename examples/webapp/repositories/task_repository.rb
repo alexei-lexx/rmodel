e634828f265724a4afcc4b1206f4cd92d5b080f7
Alexei L <alexei.lexx@gmail.com>
2016-04-22 18:36:43 +0300

    improve webapp example

--- examples/webapp/repositories/task_repository.rb
+++ examples/webapp/repositories/task_repository.rb
@@ -1,4 +1,14 @@
 class TaskRepository < Rmodel::Repository
   scope(:by_title) { order_by(:title.asc) }
   scope(:by_recency) { order_by(:created_at.desc) }
+
+  def sorted(sort_by)
+    if sort_by == 'title'
+      fetch.by_title
+    elsif sort_by == 'recency'
+      fetch.by_recency
+    else
+      fetch.by_title
+    end
+  end
 end
--- examples/webapp/server.rb
+++ examples/webapp/server.rb
@@ -2,39 +2,24 @@ require 'sinatra'
 require './lib/rmodel'
 require './examples/webapp/models/task'
 require './examples/webapp/repositories/task_repository'
+require './examples/webapp/support/dependencies'
 
-DB = Mongo::Client.new(['localhost'], database: 'test')
-task_source = Rmodel::Mongo::Source.new(DB, :tasks)
-
-task_mapper = Rmodel::Mongo::Mapper.new(Task)
-                                   .define_attribute(:title)
-                                   .define_timestamps
-
-task_repo = TaskRepository.new(task_source, task_mapper)
+include Dependencies
 
 get '/' do
-  tasks = task_repo.fetch
-
-  tasks = if params[:sort_by] == 'title'
-            tasks.by_title
-          elsif params[:sort_by] == 'recency'
-            tasks.by_recency
-          else
-            tasks.by_title
-          end
-
-  erb :index, locals: { tasks: tasks.to_a }
+  tasks = task_repo.sorted(params[:sort_by])
+  erb :index, locals: { tasks: tasks }
 end
 
 post '/' do
   task = Task.new
-  task.title = params['title']
+  task.title = params[:title]
 
   if task.valid?
     task_repo.save(task)
     redirect to "/?sort_by=#{params[:sort_by]}"
   else
-    tasks = task_repo.fetch.to_a
+    tasks = task_repo.sorted(params[:sort_by])
     erb :index, locals: { tasks: tasks }
   end
 end
